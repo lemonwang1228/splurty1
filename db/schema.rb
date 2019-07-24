0bb7a86e14bbc4a4e10ac63638c71c51c43c8cce
Lumeng <lemonwang1228@gmail.com>
2019-07-24 23:57:45 +0000

    pushing new data

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_23_225309) do
+ActiveRecord::Schema.define(version: 2019_07_24_235525) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "codes", force: :cascade do |t|
+  create_table "quotes", force: :cascade do |t|
     t.string "saying"
     t.string "author"
     t.datetime "created_at", null: false
