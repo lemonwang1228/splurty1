fca9e51773053a7094c5fe739ba480cff06b197c
Lumeng <lemonwang1228@gmail.com>
2019-08-06 20:56:39 +0000

    validating user data

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,4 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maximun: 140, minium: 3}
+  validates :author, presence: true, length: { maximum: 50, minimum: 3}
 end
